305df7dc8ab3232e414b7344d793bb9848726cd0
max-ruzhytskyi <maksym@eloops.com>
2022-08-13 13:33:13 +0200

    fixed text for modal

--- resources/views/components/modal-delete.blade.php
+++ resources/views/components/modal-delete.blade.php
@@ -5,7 +5,7 @@
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
             <div class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
-                <h6 class="text-xl font-semibold text-gray-500 lg:text-2xl dark:text-white">
+                <h6 class="text-xl font-semibold text-gray-500 dark:text-white">
                     Do you want to delete this record?
                 </h6>
                 <button type="button" class="close-modal text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="delete-modal">
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -6,7 +6,7 @@
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
             <div class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
-                <h6 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
+                <h6 class="text-xl font-semibold text-gray-900 dark:text-white">
                     Edit
                 </h6>
                 <button type="button" class="close text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="default-modal">
